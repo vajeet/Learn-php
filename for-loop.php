0fce9c13842d389cf822d82b6a2ad8c5cb677b1f
ajeet <vajeetkr@gmail.com>
2023-09-17 22:04:54 +0530

    first program in php

---
+++ for-loop.php
@@ -0,0 +1,9 @@
+<?php
+
+for($a = 1; $a <= 10; $a++){
+
+    echo $a . ") Hello Rising star<br>";    
+}
+
+
+?>
---
+++ html.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>php</title>
+    </head>
+    <body>
+     <?php
+      echo "Hello Rising";
+     ?>
+    </body>
+</html>
